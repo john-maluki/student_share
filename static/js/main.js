723931379e5eeac59452884e58f7c749ce084cb1
john <johnmaluki58@gmail.com>
2023-07-05 13:25:41 +0300

    Enable video and topic form toggling
    Clicking on share video or topic button opens the
    modal with the respective form. Also, clicking on X
    icon close modal

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,39 @@
+const topicShareButtonNode = document.querySelector("#topic-share-button");
+const videoShareButtonNode = document.querySelector("#video-share-button");
+const topicFormModalPaneNode = document.querySelector(
+  "#share-topic-form-modal"
+);
+const videoFormModalPaneNode = document.querySelector(
+  "#share-video-form-modal"
+);
+const modalTopicCloseNode = document.querySelector("#modal__topic-close");
+const modalVideoCloseNode = document.querySelector("#modal__video-close");
 
+// Handlers
+const handleTopicModalPaneToggle = (e) => {
+  e.preventDefault();
+  const isOpen = topicFormModalPaneNode.classList.contains("modal--visible");
+
+  if (isOpen) {
+    topicFormModalPaneNode.classList.remove("modal--visible");
+  } else {
+    topicFormModalPaneNode.classList.add("modal--visible");
+  }
+};
+
+topicShareButtonNode.addEventListener("click", handleTopicModalPaneToggle);
+modalTopicCloseNode.addEventListener("click", handleTopicModalPaneToggle);
+
+const handleVideoModalPaneToggle = (e) => {
+  e.preventDefault();
+  const isOpen = videoFormModalPaneNode.classList.contains("modal--visible");
+
+  if (isOpen) {
+    videoFormModalPaneNode.classList.remove("modal--visible");
+  } else {
+    videoFormModalPaneNode.classList.add("modal--visible");
+  }
+};
+
+videoShareButtonNode.addEventListener("click", handleVideoModalPaneToggle);
+modalVideoCloseNode.addEventListener("click", handleVideoModalPaneToggle);
