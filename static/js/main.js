bc5c56cdce2a1c534376da2a1e577300a13bb273
john <johnmaluki58@gmail.com>
2023-07-06 08:11:29 +0300

    Enable listing videos from the server and sorting them with most liked vides appearing on top

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,4 @@
-const MAIN_URL = "http://localhost:3000/characters";
+const MAIN_URL = "http://localhost:3000";
 
 let users = {};
 let videos = {};
@@ -25,6 +25,15 @@ const containerCardNode = document.querySelectorAll(".container-card");
 // Get methods
 const getDomainUrl = () => {
   return MAIN_URL;
+};
+
+// Set methods
+const setUsers = async () => {
+  users = await fetchAllUsersFromServer();
+};
+
+const setVideos = async () => {
+  videos = await fetchAllVideosFromServer();
 };
 
 // Handlers
@@ -80,9 +89,6 @@ const handleDisplayingVideosContent = (e) => {
   videos.classList.add("modal--visible");
 };
 
-//rest functions
-// const
-
 // Adding event listers to nodes
 videoShareButtonNode.addEventListener("click", handleVideoModalPaneToggle);
 modalVideoCloseNode.addEventListener("click", handleVideoModalPaneToggle);
@@ -94,16 +100,130 @@ homeNavNode.addEventListener("click", handleDisplayingHomeContent);
 topicsNavNode.addEventListener("click", handleDisplayingTopicsContent);
 videosNavNode.addEventListener("click", handleDisplayingVideosContent);
 
-const init = () => {
-  const home = document.querySelector("#videos_modal");
-  home.classList.add("modal--visible");
-};
-
 // Working with videos
 const fetchAllVideosFromServer = async () => {
-  return fetch(getDomainUrl())
+  return fetch(`${getDomainUrl()}/videos`)
     .then((resp) => resp.json())
     .then((videos) => videos);
+};
+
+const sortVideosByNumberOfLikes = (videos) => {
+  // sort videos based on likes
+  const sortedVideos = videos.sort((v1, v2) =>
+    v1.likes < v2.likes ? 1 : v1.likes > v2.likes ? -1 : 0
+  );
+  return sortedVideos;
+};
+
+const renderVideosOnDom = async () => {
+  const videos = await fetchAllVideosFromServer();
+  const sortedVideos = sortVideosByNumberOfLikes(videos);
+  const containerCardContentNode = document.querySelector(
+    "#container-card__content"
+  );
+  sortedVideos.forEach((video) => {
+    const user = users.find((user) => user.id === video.userId);
+
+    const userCardNode = document.createElement("div");
+    userCardNode.classList.add("user-card");
+    const userCardProfileNode = document.createElement("div");
+    userCardProfileNode.classList.add("user-card__profile");
+    const imgProfileNode = document.createElement("img");
+    imgProfileNode.classList.add("img-profile");
+    imgProfileNode.src = user.profile_pic;
+    imgProfileNode.alt = user.name;
+    const userCardDetailsNode = document.createElement("div");
+    userCardDetailsNode.classList.add("user-card__details");
+    const userCardHeaderNode = document.createElement("div");
+    userCardHeaderNode.classList.add("user-card__header");
+    const userCardTitleNode = document.createElement("h1");
+    userCardTitleNode.classList.add("user-card__title");
+    userCardTitleNode.textContent = video.title;
+    const userCardHandleNode = document.createElement("span");
+    userCardHandleNode.classList.add("user-card__handle");
+    userCardHandleNode.textContent = user.handle;
+    const userCardDescriptionNode = document.createElement("p");
+    userCardDescriptionNode.classList.add("user-card__description");
+    userCardDescriptionNode.textContent = video.video_description;
+    const userCardVideoNode = document.createElement("div");
+    userCardVideoNode.classList.add("user-card__video");
+    const iframeNode = document.createElement("iframe");
+    iframeNode.width = "100%";
+    iframeNode.height = "100%";
+    iframeNode.src = video.video_url;
+    iframeNode.title = "YouTube video player";
+    iframeNode.frameborder = "0";
+    iframeNode.allow =
+      "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share";
+    iframeNode.allowFullscreen = true;
+    const userCardFooterNode = document.createElement("div");
+    userCardFooterNode.classList.add("user-card__footer");
+    const userCardExpressionNode = document.createElement("div");
+    userCardExpressionNode.classList.add("user-card__expression");
+    const faThumbsUpNode = document.createElement("i");
+    faThumbsUpNode.classList.add("fa");
+    faThumbsUpNode.classList.add("fa-thumbs-up");
+    faThumbsUpNode.classList.add("user-card__user-expression-icon");
+    faThumbsUpNode.title = "Follow";
+    // faThumbsUpNode.aria-hidden="true"
+    const likesNode = document.createElement("h3");
+    likesNode.classList.add("user-card__expression-number");
+    likesNode.textContent = "Likes ";
+    const likesNumberNode = document.createElement("span");
+    likesNumberNode.textContent = video.likes;
+
+    const userCardExpressionNode2 = document.createElement("div");
+    userCardExpressionNode2.classList.add("user-card__expression");
+    const faUserNode = document.createElement("i");
+    faUserNode.classList.add("fa");
+    faUserNode.classList.add("fa-user");
+    faUserNode.classList.add("user-card__user-expression-icon");
+    faUserNode.title = "Follow";
+    // faUserNode.aria-hidden="true"
+    const followersNode = document.createElement("h3");
+    followersNode.classList.add("user-card__expression-number");
+    followersNode.textContent = "Followers ";
+    const followersNumberNode = document.createElement("span");
+    followersNumberNode.textContent = video.followers;
+
+    userCardNode.appendChild(userCardProfileNode);
+    userCardNode.appendChild(userCardDetailsNode);
+    userCardProfileNode.appendChild(imgProfileNode);
+
+    userCardDetailsNode.appendChild(userCardHeaderNode);
+    userCardDetailsNode.appendChild(userCardDescriptionNode);
+    userCardDetailsNode.appendChild(userCardVideoNode);
+    userCardDetailsNode.appendChild(userCardFooterNode);
+    userCardHeaderNode.appendChild(userCardTitleNode);
+    userCardHeaderNode.appendChild(userCardHandleNode);
+    userCardVideoNode.appendChild(iframeNode);
+    userCardFooterNode.appendChild(userCardExpressionNode);
+    userCardFooterNode.appendChild(userCardExpressionNode2);
+    userCardExpressionNode.appendChild(faThumbsUpNode);
+    userCardExpressionNode.appendChild(likesNode);
+    likesNode.appendChild(likesNumberNode);
+    userCardExpressionNode2.appendChild(faUserNode);
+    userCardExpressionNode2.appendChild(followersNode);
+    followersNode.appendChild(followersNumberNode);
+
+    containerCardContentNode.appendChild(userCardNode);
+  });
+};
+
+// Working with users
+const fetchAllUsersFromServer = async () => {
+  return fetch(`${getDomainUrl()}/users`)
+    .then((resp) => resp.json())
+    .then((users) => users);
+};
+
+// Initialize app when it loads
+const init = async () => {
+  const home = document.querySelector("#videos_modal");
+  home.classList.add("modal--visible");
+  await setUsers();
+  await setVideos();
+  renderVideosOnDom();
 };
 
 window.onload = init;
